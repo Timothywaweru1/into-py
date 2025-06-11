41b8bd3a6769f4839da96c99561d1e1d9abe9c79
Timothy <timothy.waweru1@student.moringaschool.com>
2025-06-11 22:11:45 +0300

    done

--- app.py
+++ app.py
@@ -1,37 +1,25 @@
 from flask import Flask
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 
-# Initialize extensions (but don't connect to app yet)
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Recommended to avoid warning also Disables unnecessary overhead
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
-    # Initialize extensions with app
-    db.init_app(app)
-    migrate = Migrate(app, db)
+@app.route('/')
+def index():
+    return "Hello world!"
 
-    # Import models after db is initialized to avoid circular imports
-    from models import User
+@app.route('/about')
+def about():
+    return "This is about us page"
 
-    @app.route('/')
-    def index():
-        return "Hello world!"
-
-    @app.route('/about')
-    def about():
-        return "This is about us page"
-
-    @app.route("/<username>")
-    def username(username):
-        return f'Hello {username}!'
-
-    return app
-
-app = create_app()
+@app.route("/<username>")
+def username(username):
+    return f'Hello {username}!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
--- models.py
+++ models.py
@@ -1,11 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer , primary_key=True)
-
+    id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String)
+    middle_name = db.Column(db.String)
     last_name = db.Column(db.String)
     username = db.Column(db.String)
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,primary_key = True)
+    post_title = db.Column(db.String, nullable=False)
+    post_content = db.Column(db.String)
